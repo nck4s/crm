4fffc60b3ee26af1a1281b2aa6d22fc4df957004
nck4s <nn.2nn@mail.ru>
2023-12-22 21:22:01 +1000

    added registration

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -27,3 +28,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST) #whatever was filled out in the form - send it here
+        if form.is_valid():
+            form.save() #saving signup form
+            #authenticate
+            username = form.cleaned_data['username'] #pulls out the username from the form
+            password = form.cleaned_data['password1'] #password1 was assigned to the variable password here
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm() #empty form
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
